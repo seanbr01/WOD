66fe8d150d1c0f663727c09a9f02b7fecd219799
Sean Bruce <seanbr01@noa.nintendo.com>
2020-04-30 09:33:43 -0700

    old work, forgot to push

---
+++ WOD/WOD_CharacterSheet_2nd/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WOD_CharacterSheet_2nd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<CheckBox> MoralityCheckBoxes { get; set; } = new List<CheckBox>();
+        public List<CheckBox> SpentWillpowerCheckBoxes { get; set; } = new List<CheckBox>();
+        MainWindowVM mainWindowVM = new MainWindowVM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (CheckBox checkBox in mainWindowVM.FindVisualChildren<CheckBox>(GetWindow(this)))
+            {
+                switch (mainWindowVM.GetControllerFamily(checkBox))
+                {
+                    case "MoralityCheckBox":
+                        MoralityCheckBoxes.Add(checkBox);
+                        break;
+                    case "SpentWillpower":
+                        SpentWillpowerCheckBoxes.Add(checkBox);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            MoralityCheckBoxes = MoralityCheckBoxes.OrderBy(cb => mainWindowVM.GetControllerValue(cb)).ToList();
+            SpentWillpowerCheckBoxes = SpentWillpowerCheckBoxes.OrderBy(cb => mainWindowVM.GetControllerValue(cb)).ToList();
+        }
+
+        private void MoralityCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            mainWindowVM.DisplayCheckBoxes(checkBox, MoralityCheckBoxes);
+        }
+
+        private void WillpowerCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            mainWindowVM.DisplayCheckBoxes(checkBox, SpentWillpowerCheckBoxes);
+        }
+    }
+}
---
+++ WOD/WOD_CharacterSheet_2nd/MainWindowVM.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WOD_CharacterSheet_2nd
+{
+    public class MainWindowVM
+    {
+        public MainWindowVM()
+        {
+            
+        }
+
+        public void BuildCheckBoxLists(DependencyObject dependencyObject)
+        {
+            foreach (CheckBox cb in FindVisualChildren<CheckBox>(dependencyObject))
+            {
+
+            }
+        }
+
+        public int GetControllerValue(CheckBox controller)
+        {
+            string[] numbers = Regex.Split(controller.Name, @"\D+");
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    int i = int.Parse(value);
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        public string GetControllerFamily(CheckBox controller)
+        {
+            Regex regex = new Regex(@"[a-zA-Z]+");
+            Match match = regex.Match(controller.Name);
+
+            return match.Value;
+        }
+
+        public void DisplayCheckBoxes(CheckBox selectedCheckBox, List<CheckBox> checkBoxes)
+        {
+            int input = GetControllerValue(selectedCheckBox);
+
+            if (selectedCheckBox.IsChecked == true)
+            {
+                foreach (CheckBox checkBox in checkBoxes)
+                {
+                    checkBox.IsChecked = (GetControllerValue(checkBox) <= input) ? true : false;
+                }
+            }
+            else
+            {
+                foreach (CheckBox checkBox in checkBoxes)
+                {
+                    checkBox.IsChecked = (GetControllerValue(checkBox) < input) ? true : false;
+                }
+            }
+        }
+
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+    }
+}
